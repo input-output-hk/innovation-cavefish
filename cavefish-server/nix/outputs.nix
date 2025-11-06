9ef73dbdf75d5dffeb67c729430fd67ff0af9a86
Nicolas Henin <mr.henin@gmail.com>
2025-11-06 15:31:40 +0200

    [nix] Add WBPS dev shell to cavefish flake

--- cavefish-server/nix/outputs.nix
+++ cavefish-server/nix/outputs.nix
@@ -19,7 +19,54 @@ let
         project
         utils
         ghc
-        ;
+      ;
+    };
+
+  wbpsDevShell =
+    pkgs.mkShell {
+      packages = with pkgs; [
+
+        gnumake
+        # The Deno runtime
+        deno
+        # JavaScript and TypeScript tools
+        typescript
+        nodejs_22
+
+        # Tools for formatting and linting
+        nodePackages.eslint_d # Fast, daemonized version of ESLint
+        nodePackages.prettier
+
+        # Python tools
+        nixfmt-rfc-style
+        pkgs.python3Packages.python-lsp-server
+        pkgs.python3Packages.pip
+        pkgs.python3Packages.click
+        pkgs.python3Packages.mypy
+        python3Packages.nox
+        python3Packages.pytest
+        python3Packages.ruff
+
+        # circom circuit compiler
+        circom
+        #
+        # nixfmt for formatting Nix code according to RFC style
+        pkgs.nixfmt-rfc-style
+        # misc tools
+        codespell
+        figlet
+      ];
+
+      shellHook = ''
+        npm install snarkjs@latest --save-dev
+        npm install vite@latest --save-dev
+
+        export PATH=$PWD/node_modules/.bin:$PATH
+        echo "WBPS dev shell" | figlet -f cybermedium
+        #
+        # to set up project
+        # deno init --npm vite --template react-ts
+      '';
     };
 
   packages = { };
@@ -30,6 +77,7 @@ let
     ghc984 = mkShell "ghc984";
     ghc9102 = mkShell "ghc9102";
     ghc9122 = mkShell "ghc9122";
+    wbps = wbpsDevShell;
   };
 
   projectFlake = project.flake { };
