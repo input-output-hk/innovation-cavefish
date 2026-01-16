acdec370b941701b8ddeaef18032b38204b9b276
Nicolas Henin <mr.henin@gmail.com>
2026-01-16 14:18:34 +0200

    Rebuild Statement from Artefacts

--- cavefish-server/wbps/tests/integration/Main.hs
+++ cavefish-server/wbps/tests/integration/Main.hs
@@ -4,6 +4,7 @@ import Test.Tasty (defaultMain, localOption, testGroup)
 import Test.Tasty.QuickCheck (QuickCheckTests (QuickCheckTests))
 import Test.Tasty.Runners (NumThreads (NumThreads))
 import WBPS.Specs.NominalCase qualified as Register.Nominal.Case
+import WBPS.Specs.Session.BlindSigning.ThetaStatement qualified as BlindSigning.ThetaStatement
 import WBPS.Specs.Session.Demonstration.Commitment.Build qualified as Commitment.BuildCommitment
 
 main :: IO ()
@@ -15,5 +16,6 @@ main =
           "Nominal Cases"
           [ localOption (NumThreads 8) . localOption (QuickCheckTests 4) $ Register.Nominal.Case.specs
           , Commitment.BuildCommitment.specs
+          , BlindSigning.ThetaStatement.specs
           ]
       ]
