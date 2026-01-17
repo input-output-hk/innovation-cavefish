ef9f94be2d4b3e3a4af341dd675bce81347ee813
kayvank <kayvan@q2io.com>
2026-01-16 21:18:05 -0800

    Add cargo and rust to dev shell
    cargo and rust-script are required to build babyjubjub-keygen

--- cavefish-server/cavefish/server/test/Core/IntentSpecs.hs
+++ cavefish-server/cavefish/server/test/Core/IntentSpecs.hs
@@ -1,11 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Core.IntentSpecs (spec) where
 
 import Cardano.Api (lovelaceToValue, serialiseAddress)
+import Data.List.NonEmpty (fromList)
 import Data.Text qualified as Text (unpack)
 import Intent.Example.DSL (
   AddressW (AddressW),
   CanonicalIntent (payTo),
-  IntentDSL (PayToW),
+  IntentDSL (AndExpsW, MaxIntervalW, MustMintW, PayToW, SpendFromW),
   toCanonicalIntent,
   unAdressConwayEra,
  )
@@ -15,15 +18,32 @@ import WBPS.Core.Registration.Artefacts.Keys.Ed25519 qualified as Ed25519 (gener
 
 spec :: Spec
 spec =
-  describe "toCanonicalIntent" $
+  describe "toCanonicalIntent" $ do
     it "converts an intent DSL into a canonical intent using a generated Ed25519 address" $ do
       Wallet {paymentAddress = (PaymentAddess expectedPaymentVerificationKey)} <- Ed25519.generateWallet
 
-      case toCanonicalIntent (PayToW (lovelaceToValue 10_000_000) (AddressW expectedPaymentVerificationKey)) of
+      case toCanonicalIntent
+        ( AndExpsW $
+            fromList
+              [ PayToW (lovelaceToValue 10_000_000) (AddressW expectedPaymentVerificationKey)
+              ]
+        ) of
         Left err -> expectationFailure (Text.unpack err)
         Right canonicalIntent ->
           case payTo canonicalIntent of
             [(outValue, outAddr)] -> do
+              print canonicalIntent
               outValue `shouldBe` lovelaceToValue 10_000_000
               serialiseAddress (unAdressConwayEra outAddr) `shouldBe` expectedPaymentVerificationKey
             other -> expectationFailure ("unexpected payTo entries: " <> show other)
+
+    it "conversion fails when the IntentDSL is invalid" $ do
+      toCanonicalIntent
+        ( AndExpsW $
+            fromList
+              [ MustMintW (lovelaceToValue 10_000_000)
+              , SpendFromW (AddressW "address-123")
+              , MaxIntervalW 10
+              ]
+        )
+        `shouldBe` Left "invalid address : address-123"
